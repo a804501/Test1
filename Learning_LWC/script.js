3186b7872613fc7943632ba4b2f19b1968e86c25
Maarten Bijvoet <maarten.bijvoet@airbus.com>
2025-08-03 10:05:50 +0000

    Variables

---
+++ Learning_LWC/script.js
@@ -0,0 +1,11 @@
+/************var keyword***************************/
+// It can be updated and also it can be re-declared
+
+var course = "zero to hero"
+console.log(window.course)
+
+function abc(){
+    var anothercourse = "hero in lwc"
+    console.log(anothercourse)
+}
+abc()
