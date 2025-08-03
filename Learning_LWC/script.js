18f10afc1137779a93974ae7dbc0cc95a156459d
Maarten Bijvoet <maarten.bijvoet@airbus.com>
2025-08-03 15:59:02 +0000

    13. Destructuring

--- Learning_LWC/script.js
+++ Learning_LWC/script.js
@@ -1,27 +1,23 @@
-/************Object/JSON Operations***************************/
+/************Destructuring***************************/
+// Destructuring means: extracting some data
 
-// We create an object with a number of properties
-let obj = {
-    name:"Salesforce",
+
+// Aray destructuring
+
+let arr = ["amazon", "google"]
+let [company1, company2] = arr // Aray destructuring
+console.log(company1)
+console.log(company2)
+
+
+// Object destructuring
+
+let options = {
+    title:"zero to hero",
     age:23,
-    dob:'23/10/1990'  
+    type:"CRM"
 }
-
-// Object operations
-
-// Extract all the keys from an object. The keys are the properties of the object.
-console.log(Object.keys(obj)) // Returns an array with all keys: ['name', 'age', 'dob']
-
-// Extract all the values from an object.
-console.log(Object.values(obj)) // Returns an array with all values: ['Salesforce', 23, '23/10/1990']
-
-// JSON operations
-
-// Convert an object into a string format = serialization
-let str = JSON.stringify(obj)
-console.log(str) 
-// returns the object as a string: {"name":"Salesforce","age":23,"dob":"23/10/1990"}
-
-// Convert the string of an object to an object = deserialization
-console.log(JSON.parse(str))
-// Deserializes the string into an object: {name: 'Salesforce', age: 23, dob: '23/10/1990'}
+let {title, age, type} = options // Object destructuring
+console.log(title)
+console.log(age)
+console.log(type)
