e4db9e7c49a50452908f3fa9d367faa38de356bc
Maarten Bijvoet <maarten.bijvoet@airbus.com>
2025-08-05 13:15:19 +0000

    30. First LWC

--- Learning_LWC/script.js
+++ Learning_LWC/script.js
@@ -1,39 +1,13 @@
-/************Arrow function***************************/
+/************setTimeout vs setInterval ***************************/
 
-// Use case 1
-
-function abc(){
+// setTimeout
+let timerId = window.setTimeout(function(){
     console.log("Hello")
-}
-abc()
+}, 2000)
+clearTimeout(timerId)
 
-const def = ()=>console.log("Hello again")
-def()
-
-// Use case 2
-
-function sum(data1,data2){
-    let sum = data1 + data2 + 10
-    return sum
-}
-console.log(sum(10,5))
-
-const add =(data1, data2)=>{
-    let sum = data1 + data2 + 10
-    return sum
-}
-console.log(add(10,5))
-
-function sum2(data1,data2){
-    return data1 + data2 + 10
-}
-console.log(sum2(10,5))
-
-const sum3 =(data1, data2)=>data1 + data2 + 10
-console.log(sum3(10,5))
-
-// Use case 3: array method map
-var arr = [1,2,3,4]
-arr.map(item){
-    return item * 2
-}
+// setInterval
+let intervalId = window.setInterval(function(){
+    console.log("Hello")
+}, 1000)
+clearInterval(intervalId)
